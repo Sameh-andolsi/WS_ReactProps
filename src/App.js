4b58e0c327f4539e3f8ccb8fe1a7bf147bec0044
Sameh-andolsi <samehandolsi02@gmail.com>
2023-02-22 11:25:26 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Profile from "./Profile/Profile";
 
 function App() {
+  const handleName = (name) => alert(name);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        style={{
+          maxWidth: "55%",
+          borderRadius: "15px",
+          padding: "15px",
+        }}
+      >
+        <Profile
+          className="prof"
+          fullName={"sameh l'andolsi"}
+          profession={"Full Stack Developer"}
+          bio="A Full Stack Developer with experience in Javascript, React,  etc...."
+          handleName={handleName}
+        ></Profile>
+        <img
+          className="image"
+          src="https://i.pinimg.com/originals/9b/e3/9c/9be39c6c000a7175483fc15f615c9c48.jpg"
+          alt=""
+          style={{ borderRadius: "15px", width: "300px" }}
+        />
+      </div>
     </div>
   );
 }
---
+++ src/Profile/Profile.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Profile = (props) => {
+
+   props.handleName(`I m  ${props.fullName} `);
+  return (
+    <div className="container">
+      <img src={props.src} alt="" />
+      {props.children}
+      <h2 className="titre">{props.fullName}</h2>
+      <h5 className="proff">I am a {props.profession}</h5>
+      <p className="biog"> {props.bio}</p>
+      <button  onClick={() => props.handleName(`I m ${props.fullName} `)}>
+        Hire Me
+      </button> 
+      </div>
+   
+  );
+};
+
+Profile.defaultProps = {
+  fullName: "Sameh L'andolsi",
+  profession: "Full Stack Developer",
+  bio: "A Full Stack Developer  ",
+};
+
+Profile.propTypes = {
+  fullName: PropTypes.string.isRequired,
+  profession: PropTypes.string,
+  bio: PropTypes.string,
+  src: PropTypes.string,
+};
+export default Profile;
